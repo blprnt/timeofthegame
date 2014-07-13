473b6364bd6130bad5708690a909de1d9048e416
Jer Thorp <blprnt@blprnt.com>
2014-07-13 14:00:11 -0400

    1 hour left.

--- node/timeofthegame.js
+++ node/timeofthegame.js
@@ -10,9 +10,13 @@ https://github.com/blprnt/timeofthegame
 
 var express = require('express'),
     stylus = require('stylus'),
-    nib = require('nib')
+    nib = require('nib'),
+    fs = require('fs')
 
 var app = express();
+var query = 'sandwich';
+
+var scraper = require('scraper.js');
 
 function compile(str, path) {
   return stylus(str)
@@ -29,13 +33,37 @@ app.use(stylus.middleware(
 ))
 app.use(express.static(__dirname + '/public'))
 
-
+//Start the server
 app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
+  var j = collectJSON(res);
+  
+})
+
+app.get('/skewtool', function (req, res) {
+  res.render('skewtool',
+  { title : 'SkewTool' }
   )
 })
-app.listen(24702)
 
+app.listen(24702)
+console.log("Listening on " + 24702)
+
+//Start the scraper
+//scraper.init(query);
+
+//Get the full JSON from the directory
+function collectJSON(res) {
+  var files = fs.readdirSync(__dirname + '/out/' + query + '/data');
+  var outs = [];
+  for (var i = 0; i < files.length; i++) {
+    var fj = JSON.parse(fs.readFileSync(__dirname + '/out/' + query + '/data/' + files[i], "utf8"));
+    if (fj.timeofgame && fj.location) outs.push(fj)
+  }
+  console.log("SENDING " + outs.length + " RECORDS.");
+  res.render('index',
+  { title : 'Home', defTogJSON:outs }
+  )
+  //return({images:outs})
+}
 
 
