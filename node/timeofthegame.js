17e15f42b10716f21730a75e8bb7b12834abbcc8
Jer Thorp <blprnt@blprnt.com>
2014-07-13 11:21:57 -0400

    Getting server up.

--- node/timeofthegame.js
+++ node/timeofthegame.js
@@ -8,30 +8,34 @@ https://github.com/blprnt/timeofthegame
 
 */
 
-var Twit = require('twit')
-var fs = require('fs')
+var express = require('express'),
+    stylus = require('stylus'),
+    nib = require('nib')
 
-var T = new Twit({
-    consumer_key: '4EP6uesxxc1g4ALu93Oh6R0UU',
-    consumer_secret: 'LRBAh8PRJN5SAdFDkrM7m5OoPh1zxc5B3f9pbJ6e2sGDrnlD8V',
-    access_token: '17013577-lq2s2tc0IVPUAftYZFO6rOm9uWeHxqV8KXCXZuZAw',
-    access_token_secret: 'rJ8mPctIdYfgaoJjOvpPe7xbpyqJVOynQeJe5zK6Ct4kc'
+var app = express();
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+//app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
 })
+app.listen(24702)
 
-//
-//  search twitter for all tweets containing the word 'banana' since Nov. 11, 2011
-//
-var outputFilename = __dirname + '/out/tog.json';
-
-T.get('search/tweets', { q: 'timeofthegame', count: 100 }, function(err, data, response) {
-  console.log(data.statuses.length)
-  fs.writeFile(outputFilename, JSON.stringify(data, null, 4), function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log("JSON saved to " + outputFilename);
-    }
-  }); 
-})
 
 
