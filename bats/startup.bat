cd3a57b3035ea9ef09821953de2ad9f001f53a6b
noa <noa@gmail.com>
2015-06-09 13:29:11 -0400

    added Ben's ip for osc testing

--- bats/startup.bat
+++ bats/startup.bat
@@ -1,9 +1,10 @@
 echo starting the osc bridge
 start cmd /k "node C:\Users\OCR\Documents\GitHub\timeofthegame\sketches\osc-web-master\bridge.js"
 echo starting the main server
+:: throw a little delay in there so everything can get setup
 timeout 2
 start cmd /k "cd C:\Users\OCR\Documents\GitHub\timeofthegame\NFM\ && http-server -p 8000"
 echo starting chrome
+:: throw a little delay in there so everything can get setup
 timeout 2
-
 start chrome --kiosk http://localhost:8000
